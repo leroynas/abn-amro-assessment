c51465bd7e9a344310dbe5cc8e8073a7b14eb731
= <=>
2022-02-03 16:19:03 +0100

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header />
+
   <router-view />
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Header from './components/layout/Header.vue';
+
+export default defineComponent({
+  components: {
+    Header,
+  },
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica,
+    Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
 }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <span class="title">Movies</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  padding: 1rem;
+  background-color: white;
+  box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+}
+
+.title {
+  font-size: 1.4rem;
+}
+</style>
