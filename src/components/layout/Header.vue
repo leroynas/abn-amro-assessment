356c1073c8b730cd6604ae0ebc812887d5115855
= <=>
2022-02-03 18:53:22 +0100

    feat: rename app title and add favicon

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="header">
-    <span class="title">Movies</span>
+    <span class="title">TV Shows</span>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .header {
-  padding: 1rem;
+  padding: 1.2rem;
   background-color: white;
   box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
 }
 
 .title {
   font-size: 1.4rem;
+  font-weight: bold;
 }
 </style>
