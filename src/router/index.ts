a8abd6d6f67a4f9c04092a1b5845a4854a9158f0
= <=>
2022-02-03 16:19:24 +0100

    refactor: rename views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,15 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
+import Dashboard from '../views/Dashboard.vue';
+import Show from '../views/Show.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    component: Dashboard,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/show/:id',
+    component: Show,
   },
 ];
 
