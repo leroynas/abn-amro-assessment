725842a77d024742fe73a19b4b0cc2d0d03714d1
= <=>
2022-02-03 18:16:35 +0100

    chore: add font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createApp } from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+library.add(faStar);
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
+
+app.component('fa-icon', FontAwesomeIcon);
 
 app.mount('#app');
